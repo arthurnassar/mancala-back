3f55d25684989b4e5c4f2ada7e0ba38abb32ef51
arthurnassar <arthurnassar@gmail.com>
2022-12-11 02:30:17 +0000

    Cors error fix test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(3000);
 }
 bootstrap();
