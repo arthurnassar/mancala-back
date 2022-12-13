267e0b353c8b7d951a58c74b5a3a326e6f27cfde
arthurnassar <arthurnassar@gmail.com>
2022-12-13 12:52:23 +0000

    Add authentication module to the imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   // TODO PREPARE TO USE ANOTHER DATABASE ON PRODUCTION
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule, GamesModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule, GamesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
