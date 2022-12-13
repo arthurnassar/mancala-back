ff296f8b065d7ed51d7e27575528851c12261fec
arthurnassar <arthurnassar@gmail.com>
2022-12-13 12:53:29 +0000

    Protect Game routes with JWT guard

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { Game } from './interface/game.interface';
 import { Game as GameSchema } from './schema/game.schema';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller()
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
   // CREATE GAME
+  @UseGuards(JwtAuthGuard)
   @Post('game')
   createGame(
     @Body()
@@ -19,18 +21,21 @@ export class GamesController {
   }
 
   // GET GAME BY ID
+  @UseGuards(JwtAuthGuard)
   @Get('game/:gameId')
   getGameById(@Param('gameId') gameId: number): Game {
     return this.gamesService.findGameById(gameId);
   }
 
   // GET GAME THAT BELONGS TO THE USER ID
+  @UseGuards(JwtAuthGuard)
   @Get('games/:userId')
   getAllGames(@Param('userId') userId: string): Game[] | void {
     return this.gamesService.findAllGames(userId);
   }
 
   // MAKE MOVE SELECTING THE PIT
+  @UseGuards(JwtAuthGuard)
   @Post('move')
   moveGamePiece(
     @Body()
