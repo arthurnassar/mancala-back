99dff06534317fc827eb41a0cae383fdcf8de161
arthurnassar <arthurnassar@gmail.com>
2022-12-13 12:54:45 +0000

    Export users repository to use on auth resource

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,6 +8,7 @@ import { UsersRepository } from './users.repository';
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository]
 })
 export class UsersModule {}
