1a67b3c7da7cc59325d15eaaa2b743224f0f60a3
arthurnassar <arthurnassar@gmail.com>
2022-12-20 11:30:32 +0000

    Cors error fix test

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
